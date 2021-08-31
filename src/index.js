c0df8faa89f1f2b8116f796b1e11927f8672842c
davi.sigon <86979947+DaviSiGon@users.noreply.github.com>
2021-08-30 23:12:53 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Botoes from './componentes/Botoes'
+
+ReactDom.render(
+    <div>
+        <Botoes/>
+    </div>
+   , document.getElementById('root')
+)
